581b96646ed5ffa9820cad0f367b99a8064a0656
tanyau-league <rxrsty@163.com>
2025-08-23 10:38:35 +0800

    Update getGithubFile.js

--- js/getGithubFile.js
+++ js/getGithubFile.js
@@ -1,6 +1,6 @@
 const apiUrl = `https://api.github.com/repos/tanyau-league/tanyau-league-data/contents/AllData.json`;
 async function getGitHubFile() {
-	let kimoi=['g', 'i', 't', 'h', 'u', 'b', '_', 'p', 'a', 't', '_', '1', '1', 'B', 'Q', 'W', 'W', 'U', 'P', 'I', '0', 'B', '7', 'p', '1', 'o', 'o', '8', 'P', 'M', 'r', 'N', 'K', '_', 'q', 'Z', 'd', '4', 'e', 'v', '2', 'd', 'J', 'J', 'W', 'B', '2', 'n', 'j', 'B', 'J', 'A', 'B', 's', 'L', 'W', 'H', 'z', 'Q', '3', 'T', '1', 'Z', 'k', 'X', 'H', '8', '8', 'R', 'v', 'd', 'B', 'e', 'M', 'n', 'd', 'a', '2', 'I', 'A', 'L', 'D', 'W', 'Z', 'U', 'p', '0', '2', 'Q', '9', 'p', '3', 'C'];
+	let kimoi=['g','i','t','h','u','b','_','p','a','t','_','1','1','B','Q','W','W','U','P','I','0','U','R','H','5','O','Y','o','J','z','5','3','3','_','v','j','C','y','K','M','H','M','p','j','g','t','I','f','0','4','z','d','P','u','I','O','e','F','e','p','7','c','w','M','W','5','G','U','Z','k','D','8','Z','V','h','i','F','O','4','H','2','Z','3','E','N','X','H','X','p','T','0','t','A'];
 	let token="";
 	for(let i=0;i<kimoi.length;i++){
 		token+=kimoi[i];
