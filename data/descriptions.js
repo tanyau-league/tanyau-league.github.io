aa9ca68b4df4d2567cc9d48152c617dd65487459
chen-SJc <robertsang@163.com>
2025-09-20 22:39:58 +0800

    Add files via upload

--- data/descriptions.js
+++ data/descriptions.js
@@ -3,7 +3,7 @@ window.playerDescriptions = {
   "Haraki": [],
   "Dawn": [],
   "Xiaobukeai": [],
-  "Koihs": [],
+  "Koihs": ["リーチ ツモ ピンフ 三色同順 ドラ4   9飜20符 倍満  8000AL"],
   "多元外卷人": [],
   "zhunzhun": [],
   "cos_squared":["リーチ ピンフ タンヤオ ドラ3   6飜30符 跳満  6000AL"],
--- data/hands.js
+++ data/hands.js
@@ -4,7 +4,7 @@ window.playerHands = {
   "Haraki": ["blank", "blank", "blank", "1m", "1m", "4m", "5m", "1m", "4m", "1p", "9p", "1p", "9p", "8p", "1p", "5z"],
   "Dawn": ["blank", "blank", "blank", "1m", "1m", "4m", "5m", "1m", "4m", "1p", "9p", "1p", "9p", "8p", "1p", "5z"],
   "Xiaobukeai": ["blank", "blank", "blank", "1m", "1m", "4m", "5m", "1m", "4m", "1p", "9p", "1p", "9p", "8p", "1p", "5z"],
-  "Koihs": ["blank", "blank", "blank", "1m", "1m", "4m", "5m", "1m", "4m", "1p", "9p", "1p", "9p", "8p", "1p", "5z"],
+  "Koihs": ["blank", "blank", "3m", "4m", "0m", "6m", "7m", "8m", "3p", "4p", "0p", "1s", "1s", "4s", "0s", "blank", "3s"],
   "多元外卷人": ["blank", "blank", "blank", "1m", "1m", "4m", "5m", "1m", "4m", "1p", "9p", "1p", "9p", "8p", "1p", "5z"],
   "zhunzhun": ["blank", "blank", "blank", "1m", "1m", "4m", "5m", "1m", "4m", "1p", "9p", "1p", "9p", "8p", "1p", "5z"],
   "cos_squared":["blank", "blank", "3m", "4m", "6m", "7m", "8m", "2p", "3p", "4p", "6p", "6p", "0s", "6s", "7s", "blank", "2m"],
