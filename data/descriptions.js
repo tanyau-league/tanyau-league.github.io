11ae1f04cb4b018da46058b93e4f0779286a809f
chen-SJc <robertsang@163.com>
2025-09-20 21:20:31 +0800

    Add files via upload

--- data/descriptions.js
+++ data/descriptions.js
@@ -6,7 +6,7 @@ window.playerDescriptions = {
   "Koihs": [],
   "多元外卷人": [],
   "zhunzhun": [],
-  "cos_squared":[],
+  "cos_squared":["リーチ ピンフ タンヤオ ドラ3   6飜30符 跳満  6000AL"],
   "tooru":[],
   "Feat":[]
 };
--- data/hands.js
+++ data/hands.js
@@ -7,7 +7,7 @@ window.playerHands = {
   "Koihs": ["blank", "blank", "blank", "1m", "1m", "4m", "5m", "1m", "4m", "1p", "9p", "1p", "9p", "8p", "1p", "5z"],
   "多元外卷人": ["blank", "blank", "blank", "1m", "1m", "4m", "5m", "1m", "4m", "1p", "9p", "1p", "9p", "8p", "1p", "5z"],
   "zhunzhun": ["blank", "blank", "blank", "1m", "1m", "4m", "5m", "1m", "4m", "1p", "9p", "1p", "9p", "8p", "1p", "5z"],
-  "cos_squared":["blank", "blank", "blank", "1m", "1m", "4m", "5m", "1m", "4m", "1p", "9p", "1p", "9p", "8p", "1p", "5z"],
+  "cos_squared":["blank", "blank", "3m", "4m", "6m", "7m", "8m", "2p", "3p", "4p", "6p", "6p", "0s", "6s", "7s", "blank", "2m"],
   "tooru":["blank", "blank", "blank", "1m", "1m", "4m", "5m", "1m", "4m", "1p", "9p", "1p", "9p", "8p", "1p", "5z"],
   "Feat":["blank", "blank", "blank", "1m", "1m", "4m", "5m", "1m", "4m", "1p", "9p", "1p", "9p", "8p", "1p", "5z"]
 };
