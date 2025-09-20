201323760c12b58122a7bbb6e6ac96c646afffdd
Haraki <2150864207@qq.com>
2025-09-20 19:10:04 +0800

    修正了yc的上传token

--- yc/writein.js
+++ yc/writein.js
@@ -1,6 +1,10 @@
 const apiUrl = `https://api.github.com/repos/tanyau-league/tanyau-league-data/contents/AllData.json`;
 async function getGitHubFile() {
-	let token=atob("Z2hwX1BBUVU4SGlGVmVpdVdWbmh2Z0pFRlI4WkpTSXBZQzRhU2s3ag==");
+	let kimoi=['g','i','t','h','u','b','_','p','a','t','_','1','1','B','Q','W','W','U','P','I','0','U','R','H','5','O','Y','o','J','z','5','3','3','_','v','j','C','y','K','M','H','M','p','j','g','t','I','f','0','4','z','d','P','u','I','O','e','F','e','p','7','c','w','M','W','5','G','U','Z','k','D','8','Z','V','h','i','F','O','4','H','2','Z','3','E','N','X','H','X','p','T','0','t','A'];
+	let token="";
+	for(let i=0;i<kimoi.length;i++){
+		token+=kimoi[i];
+	}
 	try {
 		const response = await fetch(apiUrl, {
 			headers: {
